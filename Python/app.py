5a25de63fd3e135b13511493c32dedd70ddf0ddc
joaco2752 <122044319@upq.edu.mx>
2025-02-24 23:11:54 -0600

    avance 24 febrero

---
+++ Python/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password='root',
+        database="db"
+    )
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM students')
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', students=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
